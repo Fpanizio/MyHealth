53d5424f5a7d4f2b3d9b30097179392fb473ec0b
Felipe Panizio <39607578+Fpanizio@users.noreply.github.com>
2022-10-17 16:45:31 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-import LoginScreen from './screen/LoginScreen';
-import MainScreen from './screen/MainScreen';
-import EditSceen from './screen/EditSceen';
+import Home from './screens/Home';
+import Login from './screens/Login';
+import CreateAccount from './screens/CreateAccount';
+import ResetPassword from './screens/ResetPassword';
+import EditVaccine from './screens/EditVaccine';
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="MainScreen" component={MainScreen} />
-        <Stack.Screen name="EditSceen" component={EditSceen} />
+      <Stack.Navigator>
+        <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="CreateAccount"
+              component={CreateAccount}
+              options={{headerShown: false}}
+            />
+          <Stack.Screen
+            name="ResetPassword"
+            component={ResetPassword}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="EditVaccine"
+            component={EditVaccine}
+            options={{headerShown: false}}
+          />
       </Stack.Navigator>
     </NavigationContainer>
   );
